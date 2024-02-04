3723f3e4563555a6cfcc63d11ee9048905de9750
omkar-2483 <kastureomkar03@gmail.com>
2024-02-04 07:11:58 +0530

    tut54 js2 file

---
+++ tut54_script2.js
@@ -0,0 +1,8 @@
+console.log("Hello World!")
+// command to run file: node tut54_script.js
+
+// some elements are only available in browser 
+// ex. alert, prompt, 
+
+//node.js is used for javascript in backend
+//javascript is also used in frontend
