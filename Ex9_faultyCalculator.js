f7ab1bee3f24b7e10df9dd11580ff5f59687d169
omkar-2483 <kastureomkar03@gmail.com>
2024-02-09 04:35:14 +0530

    tu59-Ex9_faultyCalculator

---
+++ Ex9_faultyCalculator.js
@@ -0,0 +1,11 @@
+/* create a faulty calculator which perform
++-->-
+--->/
+/-->**
+*-->+
+
+it perform wrong operation only 10% of Times */
+
+console.log("Enter first number: ");
+num1=InputEvent
+console.log("Enter second number: ");
