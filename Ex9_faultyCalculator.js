ca0e8ee08644af0f74f9216ad266cacd8266b331
omkar-2483 <kastureomkar03@gmail.com>
2024-02-09 04:45:04 +0530

    Ex9_faultyCalculator

--- Ex9_faultyCalculator.js
+++ Ex9_faultyCalculator.js
@@ -6,6 +6,17 @@
 
 it perform wrong operation only 10% of Times */
 
-console.log("Enter first number: ");
-num1=InputEvent
-console.log("Enter second number: ");
+let num1=prompt("Enter first number: ");
+let num2=prompt("Enter second number: ");
+let wrong=Math.random();
+if(wrong<=0.1){
+    console.log("Addition:",num1-num2);
+    console.log("Substraction:",num1/num2);
+    console.log("Division:",num1**num2);
+    console.log("Multiplication:",num1+num2);
+}else{
+    console.log("Addition:",num1+num2);
+    console.log("Substraction:",num1-num2);
+    console.log("Division:",num1/num2);
+    console.log("Multiplication:",num1*num2);
+}
