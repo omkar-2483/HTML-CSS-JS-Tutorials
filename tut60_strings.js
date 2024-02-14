22d2dcd5e5d824e75b3f993f312993a62be8f72e
omkar-2483 <kastureomkar03@gmail.com>
2024-02-15 05:15:24 +0530

    tut60_strings

---
+++ tut60_strings.js
@@ -0,0 +1,44 @@
+let a="omkar";
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+
+console.log(a.length)
+let b='nil'
+console.log("may name is"+a+"and my firend is"+b);
+console.log("may name is",a,"and my firend is",b);
+
+// template literal
+console.log(`my name is ${a} and my friend is ${b}`);
+
+// console.log("omk"ar");
+console.log(`omk"ar`)
+console.log("\nomk\"ar")
+
+//properties
+let c="shubham";
+console.log(c.toUpperCase());
+console.log(c);
+console.log(c.toLowerCase());
+console.log(c.length);
+
+//string slicing
+console.log(c.slice(1,4)) //index 1 to 3
+console.log(c.slice(1))
+c='shubhamsh'
+console.log(c.replace('sh',23)) //only once
+
+//concate
+console.log(c.concat(a,b))
+
+//remove white spaces
+str='   omkar   '
+console.log(str);
+console.log(str.trim());
+
+//strings are immutable
+str='kasture';
+console.log(str);
