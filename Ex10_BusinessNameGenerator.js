4eb6aac3b37ffb5e5a8d0142232d53823472e84f
omkar-2483 <kastureomkar03@gmail.com>
2024-02-19 05:14:03 +0530

    Ex10_BusinessNameGenerator

---
+++ Ex10_BusinessNameGenerator.js
@@ -0,0 +1,18 @@
+// create a business name generator by combining 
+// list of adjectives and shop name ond another wod
+
+/*Adjectives:
+Crazy
+Amazing
+Fire
+
+shop name:
+Engine
+Food
+garments
+
+Another word:
+Bros
+Limited
+Hub*/
+
