3d23d2b3b383f9abb4f979056eb97052a826a71c
omkar-2483 <kastureomkar03@gmail.com>
2024-02-19 05:26:23 +0530

    Ex10_BusinessNameGenerator

--- Ex10_BusinessNameGenerator.js
+++ Ex10_BusinessNameGenerator.js
@@ -16,3 +16,32 @@ Bros
 Limited
 Hub*/
 
+let rand=Math.random();
+let first,second,third;
+if(rand<0.33){
+    first="Crazy";
+}else if(rand<0.66){
+    first="Amazing";
+}else{
+    first="Fire"
+}
+
+rand=Math.random();
+if(rand<0.33){
+    second="Engine";
+}else if(rand<0.66){
+    second="Food";
+}else{
+    second="Garment"
+}
+
+rand=Math.random();
+if(rand<0.33){
+    third="Bros";
+}else if(rand<0.66){
+    third="Limited";
+}else{
+    third="HUb";
+}
+
+console.log(`${first} ${second} ${third}`);
