ef4717ca7caf8557fb35629f17eca91e768c3da5
omkar-2483 <kastureomkar03@gmail.com>
2024-04-05 04:52:59 +0530

    L10_classAndObject

---
+++ L10_classAndObject.js
@@ -0,0 +1,45 @@
+// let obj={
+//     name:'omkar',
+//     marks: 97.85,
+//     getMarks: function(){
+//         console.log('marks:',this.marks)
+//     }
+// }
+// obj.getMarks();
+
+
+
+// const employee={
+//     calTax() {
+//         console.log("tax rate is 10%");
+//     }
+// }
+// const karan ={
+//     salary:10000,
+//     calTax() {
+//         console.log("tax rate is 20%");
+//     }
+// }
+// karan.__proto__ = employee;
+
+
+// class ToyotaCar {
+//     constructor(){
+//         console.log('creating new object')
+//     }
+// }
+
+// let fortuner = new ToyotaCar();
+// let lexus = new ToyotaCar();
+
+a=10;
+b=15;
+console.log('a+b',a+b);
+console.log('a+b',a+b);
+try{
+    console.log('a+b',a+c);
+}catch(err){
+    console.log(err);
+}
+console.log('a+b',a+b);
+console.log('a+b',a+b);
