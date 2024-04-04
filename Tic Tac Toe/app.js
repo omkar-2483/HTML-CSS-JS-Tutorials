3bc21d93d00f382337ec54e8b97fbeefe2bf5726
omkar-2483 <kastureomkar03@gmail.com>
2024-04-04 05:54:00 +0530

    tic tac toe game

--- Tic Tac Toe/app.js
+++ Tic Tac Toe/app.js
@@ -1,5 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn=document.querySelector('#new-btn');
+let msgContainer=document.querySelector(".msg-container");
+let msg=document.querySelector('#msg')
 
 let turnO = true; //to track turn of playerX,playerO
 const winPatterns = [
@@ -12,6 +15,15 @@ const winPatterns = [
   [3, 4, 5],
   [6, 7, 8],
 ];
+
+const resetGame =() =>{
+  turn0=true;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+}
+
+newGameBtn.addEventListener("click",resetGame);
+resetBtn.addEventListener("click",resetGame);
 
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
@@ -29,6 +41,25 @@ boxes.forEach((box) => {
   });
 });
 
+const showWinner = (winner) =>{
+  msg.innerText=`Congratulations! Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+}
+
+const disableBoxes=()=>{
+  for(let box of boxes){
+    box.disabled=true;
+  }
+}
+
+const enableBoxes=()=>{
+  for(let box of boxes){
+    box.disabled=false;
+    box.innerText="";
+  }
+}
+
 const checkWinner = () => {
   for (let pattern of winPatterns) {
     let pos1Val=boxes[pattern[0]].innerText;
@@ -37,8 +68,11 @@ const checkWinner = () => {
 
     if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
         if(pos1Val===pos2Val && pos2Val===pos3Val){
-            console.log("winner: ",pos1Val)
+            // console.log("winner: ",pos1Val)
+            showWinner(pos1Val);
+
         }
     }
   }
 };
+
